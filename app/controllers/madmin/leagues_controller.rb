36ee276b212bc4b5b41562e62d44de3b98f13f4d
Joshua LeBlanc <jleblanc@hey.com>
2026-05-12 07:14:26 -0300

    fix: use correct oath for redirect

--- app/controllers/madmin/leagues_controller.rb
+++ app/controllers/madmin/leagues_controller.rb
@@ -2,12 +2,20 @@ module Madmin
   class LeaguesController < Madmin::ResourceController
     def start_season
       league = League.find(params[:id])
-      if league.accepting_signups?
-        LeagueActivationService.new(league).activate!
-        redirect_to madmin_league_path(league), notice: "Season started! Groups and matches created."
-      else
-        redirect_to madmin_league_path(league), alert: "League must be accepting signups to start the season."
+
+      unless league.accepting_signups?
+        redirect_to main_app.madmin_league_path(league),
+                    alert: "League must be accepting signups to start the season."
+        return
       end
-    end 
+
+      if LeagueActivationService.new(league).activate!
+        redirect_to main_app.madmin_league_path(league),
+                    notice: "Season started! Groups and matches created."
+      else
+        redirect_to main_app.madmin_league_path(league),
+                    alert: "Could not start season: #{league.errors.full_messages.to_sentence.presence || 'unknown error'}."
+      end
+    end
   end
 end
